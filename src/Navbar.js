ff8c54f1e9203ff5c7e3b6a5458c9158317e1450
Abdulqudus Ibrahim <abdulaiko16@gmail.com>
2022-12-08 19:20:04 +0100

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar(){
+
+    return <div style = {{backgroundColor:'black'}}>
+        <h1 style={{color:'white'}}>REACT MEALS</h1>
+        <a style={{margin:'10px',color:'white'}}>HOME</a>
+        <a style={{margin:'10px',color:'white'}}>ORDER</a>
+        <a style={{margin:'10px',color:'white'}}>CONTACT US</a>
+    </div>
+  
+}
+export default Navbar
